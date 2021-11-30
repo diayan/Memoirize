505cccf18e1f8186b762f10712e3f5b599a90664
diayan <diayans@outlook.com>
2021-11-30 17:38:47 +0000

    Initial Commit

---
+++ Memoirize/MemoirizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoirizeApp.swift
+//  Memoirize
+//
+//  Created by diayan siat on 30/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoirizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
